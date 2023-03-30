b9c57e6e11c769f9c8982680f344ab8d57cd72d7
basavarajakj2000 <basavarajakj06@gmail.com>
2023-03-30 12:37:56 +0530

    local module

--- Basics/02-helloworld.js
+++ Basics/02-helloworld.js
@@ -1,1 +1,10 @@
 console.log("Hello world!") //Hello world!
+
+/**
+ * Browser V/S Node.js
+ * 
+ *  => In browser is interacting with the DOM, or other Web Platform API likes Cookies
+ *  => In browser no file system access functionality
+ * 
+ *  => Node.js can control the environment like we can write ES6 ES7 so and so far
+ */
---
+++ Basics/03-modules/01-intro.js
@@ -0,0 +1,12 @@
+/**
+ * MODULES:
+ *  A module is an encapsulated and reusable chunk of code that has its own context in Node.js
+ *  each file is treated as a separate module
+ * 
+ * 
+ * TYPES of MODULE
+ * 
+ *  1. Local module - module tht we create in our application
+ *  2. Built-in module - Module that Node.js ships with out of the box
+ *  3. Third party module - Module written by other developers that we can use in our application
+ */
