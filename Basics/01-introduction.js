6efd51fc4346925190e3702d97dac6a6a74c3eab
basavarajakj2000 <basavarajakj06@gmail.com>
2023-03-30 11:08:36 +0530

    first commit

---
+++ Basics/01-introduction.js
@@ -0,0 +1,25 @@
+/**
+ * Node.js is an open-source, cross-platform Javascript runtime environment
+ *
+ *  => Open-source    - source code is publicly available to sharing and modification
+ *  => Cross-platform - Available on Mac, Windows and Linux.
+ *  => JS runtime environment - ?
+ */
+
+/**
+ * Why learn Node.js
+ * 
+ *  => Build end-to-end JS application
+ *  => Major companies like Linkedln, Netflix,Paypal
+ *  => Full stack is most sought out skill
+ *  => Huge community support
+ */
+
+/**
+ * JS Engine : program to execute JS code
+ * 
+ * Chrome V8 engine: Open-source JS engine developed by Google for chrome. written in C++
+ * SpiderMonkey: Mozilla Firefox
+ * JavaScriptCore: Apple for safari
+ * Chakra: Microsoft for edge
+ */
